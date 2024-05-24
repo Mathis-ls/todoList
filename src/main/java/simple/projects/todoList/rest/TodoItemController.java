f804cb635453fb5bd143e80697377823e0e2200e
mathis-ls <mathis.schott@gmail.com>
2024-05-24 11:44:51 +0200

    fix patchmapping and putmapping

--- src/main/java/simple/projects/todoList/rest/TodoItemController.java
+++ src/main/java/simple/projects/todoList/rest/TodoItemController.java
@@ -1,12 +1,15 @@
 package simple.projects.todoList.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import simple.projects.todoList.entity.Priority;
 import simple.projects.todoList.entity.TodoItem;
 import simple.projects.todoList.service.TodoItemService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/todoitems")
@@ -35,8 +38,36 @@ public class TodoItemController {
     }
 
     @PutMapping("/{id}")
-    public TodoItem setPriority(@PathVariable int id, @RequestBody Priority priority){
-        return todoItemService.changePriority(id,priority);
+    public TodoItem updateTodoItem(@PathVariable int id, @RequestBody TodoItem updateTodoItem){
+        TodoItem foundItem = todoItemService.findById(id);
+        if(foundItem == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TodoItem not found");
+
+        foundItem.setPriority(updateTodoItem.getPriority());
+        foundItem.setContent(updateTodoItem.getContent());
+
+        todoItemService.save(foundItem);
+        return foundItem;
+    }
+
+    @PatchMapping("/{id}")
+    public TodoItem updateTodoItemFields(@PathVariable int id, @RequestBody Map<String, Object> updates){
+        TodoItem todoItem = todoItemService.findById(id);
+        if (todoItem == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TodoItem not found");
+        }
+
+        if (updates.containsKey("content")) {
+            String newContent = (String) updates.get("content");
+            todoItem.setContent(newContent);
+        }
+
+        if (updates.containsKey("priority")) {
+            String newPriority = (String) updates.get("priority");
+            todoItem.setPriority(Priority.valueOf(newPriority.toUpperCase()));
+        }
+
+        todoItemService.save(todoItem);
+        return todoItem;
     }
 
     @DeleteMapping("/{id}")
